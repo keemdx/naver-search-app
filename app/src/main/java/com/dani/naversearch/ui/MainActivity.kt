752e1785df2800ab3d6dedcc8513e26e31e43ded
devdanikim <devdanikim@gmail.com>
2021-08-31 16:12:57 +0900

    Initial commit

---
+++ app/src/main/java/com/dani/naversearch/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.dani.naversearch.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dani.naversearch.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
